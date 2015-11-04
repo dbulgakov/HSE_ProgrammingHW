199bbf2a04d52cbf1cb29b7d334404aeda9018ac
Dmitry Bulgakov <dbulgakov@users.noreply.github.com>
2015-11-04 12:50:46 +0300

    Added TestRepository class (no implemention yet)

---
+++ StudentRating.Classes/Repositories/TestRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StudentRating.Classes.Interfaces;
+
+namespace StudentRating.Classes.Repositories
+{
+    class TestRepository : IRepository
+    {
+        public List<Domain.Grade> Grades
+        {
+	        get { throw new NotImplementedException(); }
+        }
+
+        public List<Domain.Course> Courses
+        {
+	        get { throw new NotImplementedException(); }
+        }
+
+        public event Action GradesChanged;
+
+        public void AddGrade(Domain.Grade grade)
+        {
+ 	        throw new NotImplementedException();
+        }
+
+        public void EditGrade(Domain.Grade grade)
+        {
+ 	        throw new NotImplementedException();
+        }
+
+        public void RemoveGrade(Predicate<Domain.Grade> p)
+        {
+ 	        throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+ 	        throw new NotImplementedException();
+        }
+    }
+}
