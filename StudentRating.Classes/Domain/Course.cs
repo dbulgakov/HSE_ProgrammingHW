ce46ca99400177354dd9da38802f98ce9e972bac
Dmitry Bulgakov <dbulgakov@users.noreply.github.com>
2015-11-04 12:40:53 +0300

    Added library files

---
+++ StudentRating.Classes/Domain/Course.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentRating.Classes.Domain
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Credits { get; set; }
+
+        public Course(string name, double credits)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentException("Course name cannot be empty");
+            if (credits <= 0)
+                throw new ArgumentException("Credits for the course should be a positive number");
+            Name = name;
+            Credits = credits;
+        }
+
+        public override bool Equals(object obj)
+        {
+            Course course = obj as Course;
+            if (course == null)
+                return false;
+            else
+                return Name.Equals(course.Name);
+        }
+    }
+}
---
+++ StudentRating.Classes/Domain/Grade.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentRating.Classes.Domain
+{
+    public class Grade
+    {
+        public int Id { get; set; }
+        public Course Course { get; set; }
+        public int Mark { get; set; }
+
+        public Grade(Course course, int mark)
+        {
+            Course = course;
+            Mark = mark;
+        }
+
+        public override bool Equals(object obj)
+        {
+            Grade grade = obj as Grade;
+            if (grade == null)
+                return false;
+            else
+                return Course == grade.Course;
+        }
+    }
+}
