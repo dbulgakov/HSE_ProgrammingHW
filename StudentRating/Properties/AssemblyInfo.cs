0f12ff859310788a0177867267a729105179b493
Dmitry Bulgakov <dbulgakov@users.noreply.github.com>
2015-11-05 16:02:36 +0300

    Updated output folder, changed icon, updated info

--- StudentRating/Properties/AssemblyInfo.cs
+++ StudentRating/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Interfaces")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Homework 3")]
+[assembly: AssemblyDescription("HW3 - Separation of responsibilities. Interfaces")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("HSE")]
 [assembly: AssemblyProduct("Interfaces")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
