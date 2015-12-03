0d7eae81700a9a850586c5d650c1a842856a1296
Dmitry Bulgakov <dbulgakov@users.noreply.github.com>
2015-12-04 00:36:36 +0300

    Publish Sources

---
+++ RandomQueue/MyQueue.cs
@@ -0,0 +1,135 @@
+﻿using System;
+
+namespace RandomQueue
+{
+    class MyQueueItem<T>
+    {
+        public T Data { get; set; }
+        public MyQueueItem<T> Next { get; set; }
+        public MyQueueItem<T> Prev { get; set; }
+    }
+
+    class MyQueue<T>
+    {
+        private MyQueueItem<T> _head;
+        private MyQueueItem<T> _tail;
+        private int _counter;
+
+        public MyQueue()
+        {
+            _counter = 0;
+        }
+        // Add a new value to the queue
+        public void Push(T item)
+        {
+            if (_head == null)
+            {
+                _head = new MyQueueItem<T> {Data = item};
+                _tail = _head;
+            }
+            else
+            {
+                MyQueueItem<T> tempItem = new MyQueueItem<T> {Data = item};
+                _tail.Next = tempItem;
+                tempItem.Prev = _tail;
+                _tail = tempItem;
+            }
+            _counter++;
+        }
+
+        // Add several new values to the queue
+        public void Push(T[] items)
+        {
+            if (items == null)
+            {
+                throw new ArgumentNullException();
+            }
+            if (items.Length == 0)
+            {
+                throw new ArgumentException();
+            }
+
+            foreach (T item in items)
+            {
+                Push(item);
+            }
+        }
+
+        // Pick a random item from the queue, remove it from the internal storage and return it
+        public T Pop()
+        {
+            if (_counter == 0)
+            {
+                throw new InvalidOperationException();
+            }
+
+            MyQueueItem<T> tmpLink = GetLinkByIndex(GetRndIndex());
+            T elementData = tmpLink.Data;
+
+            ReLinkItems(tmpLink);
+
+            _counter--;
+            return elementData;
+
+        }
+
+        // Number of stored items
+        public int Count
+        {
+            get { return _counter; }
+        }
+
+        // Number of allocated slots for storing items
+        public int Capacity
+        {
+            get { return _counter; }
+        }
+
+        // Boolean flag: true if no items are stored in the queue, false otherwise
+        public bool IsEmpty
+        {
+            get { return _counter == 0; }
+        }
+
+        private int GetRndIndex()
+        {
+            Random r = new Random();
+            return r.Next(0, _counter);
+        }
+
+        private MyQueueItem<T> GetLinkByIndex(int index)
+        {
+            MyQueueItem<T> tmpLink = _head;
+
+            for (int i = 0; i < index; i++)
+            {
+                tmpLink = tmpLink.Next;
+            }
+
+            return tmpLink;
+        }
+
+        private void ReLinkItems(MyQueueItem<T> item)
+        {
+            if (item.Prev == null && item.Next == null)
+            {
+                _head = _tail = null;
+            }
+            else if (item.Prev == null)
+            {
+                item.Next.Prev = null;
+                _head = item.Next;
+            }
+            else if (item.Next == null)
+            {
+                item.Prev.Next = null;
+                _tail = item.Prev;
+            }
+            else
+            {
+                item.Next.Prev = item.Prev;
+                item.Prev.Next = item.Next;
+            }
+        }
+    }
+}
---
+++ RandomQueue/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyQueue<int> queue = new MyQueue<int>();
+
+            queue.Push(10);
+            queue.Push(20);
+            queue.Push(30);
+            queue.Push(new int[] { 40, 50, 60 });
+
+            Console.WriteLine("Elements in random order:");
+            while (!queue.IsEmpty)
+            {
+                Console.WriteLine(queue.Pop());
+            }
+            Console.ReadKey();
+        }
+    }
+}
