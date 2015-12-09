2161fcc7035fa6f24afa6225706c48f52a10b57c
Dmitry Bulgakov <dbulgakov@users.noreply.github.com>
2015-12-09 04:01:52 +0300

    Some refactoring

--- FileSearch/SearchEngine.cs
+++ FileSearch/SearchEngine.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using FileSearch.Properties;
@@ -57,7 +54,7 @@ namespace FileSearch
             }
 
 
-            catch
+            catch (Exception)
             {
             }
         }
