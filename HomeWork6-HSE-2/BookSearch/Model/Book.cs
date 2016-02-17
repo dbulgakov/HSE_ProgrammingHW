070b691d58b66ac99c28298fad1d28fc0f7ec76a
Dmitry Bulgakov <mail@dbulgakov.com>
2016-02-18 02:59:08 +0300

    Added final model

---
+++ HomeWork6-HSE-2/BookSearch/Model/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookSearch.Model
+{
+    class Book
+    {
+        public string Title { get; set; }
+        public string SubTitle { get; set; }
+        public string Language { get; set; }
+        public int PublishDate { get; set; }
+        public string WebReaderLink { get; set; }
+        public string Thumbnail { get; set; }
+    }
+}
